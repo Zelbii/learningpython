e72500825c3bdfe6b851029bd93356356862ea89
Zelbii <99200003+Zelbii@users.noreply.github.com>
2022-02-10 15:49:18 +0000

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,7 @@
+#fazer um programa que leia algo que foi escrito e motre o seu tipo primitivo e algumas outras informações.
+a=input('Digite algo')
+print('o tipo primitivo desse valor é ',type(a))
+print('só tem espaço?',a.isspace())
+print('é numero?',a.isnumeric())
+print('é alfabetico?',a.isalpha())
+print('é alfanumérico?',a.isalpha())
