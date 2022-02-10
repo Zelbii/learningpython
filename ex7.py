eb9e43c23067f21c8b1b145264b5edae52ecc204
Zelbii <99200003+Zelbii@users.noreply.github.com>
2022-02-10 17:41:54 +0000

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,5 @@
+#esse programa é pra devolver a média de um aluno 
+n1=float(input('insira a nota do teu primeiro teste '))
+n2=float(input('insira a nota do teu segundo teste '))
+n3=(n1+n2)/2
+print('a nota do teu primeiro teste foi {}, já o segundo foi {}, sendo assim a tua média é de {}'.format(n1,n2,n3))
