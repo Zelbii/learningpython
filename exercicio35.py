fcc105d49d217bb7ede09d9f36eaabe8d4612f07
Zelbii <99200003+Zelbii@users.noreply.github.com>
2022-02-20 20:23:52 +0000

    Delete exercicio35.py

--- exercicio35.py
+++
@@ -1,7 +0,0 @@
-s1=float(input('o primeiro segmento tem '))
-s2=float(input('o segundo segmento tem '))
-s3=float(input('o terceiro segmento tem '))
-if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
-    print('Os segmentos acima podem formar um trinângulo!')
-else:
-    print('Os segmentos acima não podem formar um triângulo!')
