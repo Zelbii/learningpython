4576e0df4a6e78e6280fe5bbcdbace3e5e79855a
Zelbii <99200003+Zelbii@users.noreply.github.com>
2022-02-22 22:46:26 +0000

    Add files via upload

---
+++ Aula12.py
@@ -0,0 +1,8 @@
+n=str(input('Qual o seu nome? ')).upper()
+if n == 'ELBER':
+    print('Você ta gordo {}'.format(n))
+elif n == 'JOANA' or n == 'JUUU':
+    print('Seu nome é lindo, assim como você {}'.format (n))
+elif n == 'SILVIA':
+    print('SILVA SILVAAAAAAAAAAAAAAAAAAAAAAAAA!')
+print('tenha um bom dia, {}'.format(n))
