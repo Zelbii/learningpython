f949594c9a7411baa3f2cdb0a067c59411cf65b6
Elber Chaves <99200003+Zelbii@users.noreply.github.com>
2022-02-27 22:34:16 +0000

    Add files via upload

---
+++ exercicio38.py
@@ -0,0 +1,8 @@
+n1=int(input('digite o primeiro númemero '))
+n2=int(input('digite o segundo númemero '))
+if n1>n2:
+    print('o Primeiro valor é maior')
+elif n2>n1:
+    print('o Segundo valor é maior')
+elif n1==n2:
+    print('Os valores são iguais')
