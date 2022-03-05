902beabdb0f6b29a246bcb808ce98927dd9a6361
Elber Chaves <99200003+Zelbii@users.noreply.github.com>
2022-03-05 17:05:30 +0000

    Update exercicio37.py

--- exercicio37.py
+++ exercicio37.py
@@ -7,10 +7,10 @@ print('''escolha uma das bases para a conversão:
 opção=int(input('Sua opção: '))
 
 if opção==1:
-    print('{} convertido para BINÀRIO é igual a {}'.format(num, bin(num)[2:]))
+    print('{} convertido para BINÀRIO é igual a {}'.format(num, bin(num)[2:])) #[2:] é para começar a contar depois do 2 número porque no python as funções BIN OCT HEX elas começam com caracteres estranhos tipo Bi110011
 elif opção==2:
     print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
 elif opção==3:
-    print('{} convertido para HEXADECIMAL é igual a {}'-format(num,hex(num)[2:]))
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num,hex(num)[2:]))
 else:
     print('opção inválida. tenta novamento')
