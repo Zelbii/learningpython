ac548619b5b96e279aa89a724e2986695c2c204b
Elber Chaves <99200003+Zelbii@users.noreply.github.com>
2022-03-05 21:20:49 +0000

    Update exercicio42.py

--- exercicio42.py
+++ exercicio42.py
@@ -2,7 +2,7 @@ s1=float(input('o primeiro segmento tem '))
 s2=float(input('o segundo segmento tem '))
 s3=float(input('o terceiro segmento tem '))
 if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
-    print('Os segmentos acima podem formar um trinângulo!', end='')
+    print('Os segmentos acima podem formar um triângulo!', end='')
     if s1==s2==s3:
         print('EQUILÁTERO ')
     elif s1!=s2!=s3!=s1:
