b8d52f2cdc21c078c220317b935104117928519b
Elber Chaves <99200003+Zelbii@users.noreply.github.com>
2022-03-06 13:59:17 +0000

    Add files via upload

---
+++ Aula 13 Laços de repetição (parte 1).py
@@ -0,0 +1,41 @@
+'''print('oi')
+print('oi')
+print('oi')
+print('oi')
+print('oi')
+print('oi')'''
+
+'''for c in range(0,6): #no piton ele vai contar do 1 até o 5 e no 6 ele para, por isso que colocamos o 0, para fazer do 5 até o 5 contando 6 vezes de OI
+    print('oi')
+
+print('fim')#vai escrever oi 6 vezes e depois dar print 'FIM'''
+
+'''for c in range(0,6):
+    print('oi')
+    print('fim') #aqui o programa vai escrever oi fim 6 vezes só porque o "print('fim')" está na mesma camada que o "print('oi')"'''
+
+'''for c in range (1,6):
+    print(c)
+print('fim')
+
+for c in range (6,0,-1):
+    print(c)
+print('FIM')'''
+
+
+'''i=int(input('início: '))
+f=int(input('Fim: '))
+p=int(input('Passo: '))
+for c in range(i,f+1,p):
+    print(c)
+print('FIM')
+
+for c in range(0,3):
+    n=int(input('Digite um valor: '))
+print('fim')'''
+
+s=0
+for c in range(0,4):
+    n=int(input('Digite um valor: '))
+    s+=n
+print(' o somatório de todos os valores foi {}'.format(s))
