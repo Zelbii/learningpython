14acaaa2f29a5a9076891b8073563de9ca7192b7
Elber Chaves <99200003+Zelbii@users.noreply.github.com>
2022-03-07 20:45:28 +0000

    Add files via upload

---
+++ ex47.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+
+for c in range(2,51,2):
+    print(c, end=' ')
+    sleep(0.5)
+print('FIM')
+
+'''for c in range (1,51):
+    if c % 2==0:
+        print(c, end=' ')
+print('FIM')'''
