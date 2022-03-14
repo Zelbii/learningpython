76a3396b4d401c599dabb0ab3726e7881a2edcde
Elber Chaves <99200003+Zelbii@users.noreply.github.com>
2022-03-14 22:25:33 +0000

    Add files via upload

---
+++ ex62.py
@@ -0,0 +1,16 @@
+print('Gerador de PA')
+print('_.-.'*19)
+n1=int(input('Primeiro termo: '))
+razão=int(input('Razão da PA: '))
+termo=n1
+cont=1
+total=0
+mais=10
+while n2!=0:
+    total=total+mais
+    while cont <=total:
+        print('{}-> '.format(termo),end='')
+        termo=termo+razão
+        cont=cont+1
+    n2=int(input('Quantos termos você quer adicionar? '))
+print('progressão finalizado com {} termos'.format(total))
