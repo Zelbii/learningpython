12765b17f17397710b52bfc73f21078708dd0f3e
Elber Chaves <99200003+Zelbii@users.noreply.github.com>
2022-03-17 19:04:33 +0000

    Update ex65.py

--- ex65.py
+++ ex65.py
@@ -7,5 +7,13 @@ while resp in 'Ss':
     resp=str(input('Quer continuar? [S/N]')).upper().strip() [0]
     soma+=num   
     cont+=1
+    if cont==1:
+        maior=menor=num
+    else:
+        if num>maior:
+            maior=num
+        if num<menor:
+            menor=num
 media=soma/cont
 print('Acabou, digitou {} vezes sendo que a soma é {} assim sendo a média é de {}'.format(cont,soma,media))
+print('O maior valor foi {} e o menor foi {}'.format(maior,menor))
